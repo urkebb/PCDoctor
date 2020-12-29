246937c7a1897c9b3ae93f4c500ddd3f44b3d7e9
urkebb <urkebb@elfak.rs>
2020-12-29 16:51:00 +0100

    komitovi

--- src/Postovi/Pages/NoviPost.js
+++ src/Postovi/Pages/NoviPost.js
@@ -1,18 +1,79 @@
-import React from 'react';
+import React, {useCallback,useReducer} from 'react';
 
 import Input from '../../Shared/Components/FormElements/Input';
+import {VALIDATOR_MINLENGTH, VALIDATOR_REQUIRE} from '../../Shared/util/validators';
+import Button from '../../Shared/Components/FormElements/Button';
 import './NoviPost.css';
 
-const NoviPost = () => {
+const formReducer = (state, action) => {
+    switch (action.type) {
+      case 'INPUT_CHANGE':
+        let formIsValid = true;
+        for (const inputId in state.inputs) {
+          if (inputId === action.inputId) {
+            formIsValid = formIsValid && action.isValid;
+          } else {
+            formIsValid = formIsValid && state.inputs[inputId].isValid;
+          }
+        }
+        return {
+          ...state,
+          inputs: {
+            ...state.inputs,
+            [action.inputId]: { value: action.value, isValid: action.isValid }
+          },
+          isValid: formIsValid
+        };
+      default:
+        return state;
+    }
+  };
+  
+  const NoviPost = () => {
+    const [formState, dispatch] = useReducer(formReducer, {
+      inputs: {
+        title: {
+          value: '',
+          isValid: false
+        },
+        description: {
+          value: '',
+          isValid: false
+        }
+      },
+      isValid: false
+    });
+  
+    const inputHandler = useCallback((id, value, isValid) => {
+      dispatch({
+        type: 'INPUT_CHANGE',
+        value: value,
+        isValid: isValid,
+        inputId: id
+      });
+    }, []);
+
     return (
         <form className="post-form">
             <Input
+            id="title"
             element ="input" 
             type="text" 
-            label="Naslov" 
-            validators={[]} 
+            label="Title" 
+            validators={[VALIDATOR_REQUIRE()]} 
             errorText="Molim vas unesite validan naslov."
+            onInput={inputHandler}
              />
+
+            <Input
+            id="description"
+            element ="textarea" 
+            label="Description" 
+            validators={[VALIDATOR_MINLENGTH(5)]} 
+            errorText="Unesite opis od najmanje 5 karaktera."
+            onInput={inputHandler}
+             />
+
             </form>
     )
 };
